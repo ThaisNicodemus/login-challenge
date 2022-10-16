9a7772260aff8e3732f22fb1b6609cf74fc5b84e
thais.nicodemus <thaisnicodemus@gmail.com>
2022-10-16 16:21:44 -0300

    styling home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -41,26 +41,64 @@ export function HomePage() {
 
 			<main className="w-screen h-fit grid grid-cols-2 bg-white">
 				<div className="flex justify-start items-start">
-					<img src={BallImg} alt="compass-logo-ball" className="w-[31.563rem] bottom-0" />
+					<img
+						src={BallImg}
+						alt="compass-logo-ball"
+						className="w-[31.563rem] bottom-0"
+					/>
 				</div>
 
 				<div className="flex flex-col justify-center items-end pr-10">
-					<Text size='2lg' color='text-red-200' className='font-bold'>Our mission is</Text>
-					<Text size='2md' color='text-black'>Nossa missão é</Text>
-					<Text size='3lg' color='text-red-200' className='font-bold'>to transform the world</Text>
-					<Text size='2md' color='text-black'>transformar o mundo</Text>
-					<Text size='3lg' color='text-red-200' className='font-bold'>building digital experiences</Text>
-					<Text size='2md' color='text-black'>construindo experiências digitais</Text>
-					<Text size='3lg' color='text-red-200' className='font-bold'>that enable our client’s growth</Text>
-					<Text size='2md' color='text-black'>que permitam o crescimento dos nossos clientes</Text>
-
+					<Text size="2lg" color="text-red-200" className="font-bold">
+						Our mission is
+					</Text>
+					<Text size="2md" color="text-black">
+						Nossa missão é
+					</Text>
+					<Text size="3lg" color="text-red-200" className="font-bold">
+						to transform the world
+					</Text>
+					<Text size="2md" color="text-black">
+						transformar o mundo
+					</Text>
+					<Text size="3lg" color="text-red-200" className="font-bold">
+						building digital experiences
+					</Text>
+					<Text size="2md" color="text-black">
+						construindo experiências digitais
+					</Text>
+					<Text size="3lg" color="text-red-200" className="font-bold">
+						that enable our client’s growth
+					</Text>
+					<Text size="2md" color="text-black">
+						que permitam o crescimento dos nossos clientes
+					</Text>
 				</div>
 			</main>
 
-			<footer className="w-screen h-24 grid grid-cols-3 absolute">
-				<div className="bg-gray-200"></div>
-				<div className="bg-primary-yellow"></div>
-				<div className="bg-gray-200"></div>
+			<footer className="w-screen grid grid-cols-3 absolute justify-center items-center bg-black">
+				<div className="flex">
+					<Text size="xs" color='text-secondary-white'>
+						Essa janela do navegador é usada para manter sua sessão de autenticação
+						ativa. Deixe-a aberta em segundo plano e abra uma nova janela para
+						continuar a navegar.
+					</Text>
+				</div>
+				<div className="flex justify-center items-center">
+					<Text size="xs" color='text-secondary-white'>Application refresh in</Text>
+					<div className="flex flex-col justify-center items-center">
+						<Text size="3lg"color='text-secondary-white'>600</Text>
+						<Text size="sm" color='text-secondary-white'>seconds</Text>
+					</div>
+				</div>
+				<div className="flex justify-center items-center">
+					<div className='bg-white'>
+						<a className='text-red-100'>Continuar Navegando</a>
+					</div>	
+					<div className="flex flex-col justify-center items-center">
+						<a className='text-secondary-white'>Logout</a>
+					</div>
+				</div>
 			</footer>
 		</div>
 	)
